ffc09e14bb675babb38a4288e48e4dd98a763d98
smackem <sm4ckem@outlook.com>
2015-06-07 21:58:13 +0200

    Added service contracts

---
+++ DrawTogether.Services/Figurecontract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawTogether.Services
+{
+    public class FigureContract
+    {
+        public FigureContract(VertexContract[] vertices, FigureKindContract kind, int argb, int userId)
+        {
+            Vertices = vertices;
+            Kind = kind;
+            Argb = argb;
+            UserId = userId;
+        }
+
+        public VertexContract[] Vertices { get; private set; }
+        public FigureKindContract Kind { get; private set; }
+        public int Argb { get; private set; }
+        public int UserId { get; private set; }
+    }
+}
